40afbfb1e6d8d60cddab2b1197bf252a574131fd
Seulah-coder <seulah.developer@gmail.com>
2025-12-09 14:21:51 +0900

    controller->service 연결

--- src/main/java/io/code/tdd/point/PointController.java
+++ src/main/java/io/code/tdd/point/PointController.java
@@ -4,14 +4,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
+import lombok.RequiredArgsConstructor;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/point")
+@RequiredArgsConstructor
 public class PointController {
 
     private static final Logger log = LoggerFactory.getLogger(PointController.class);
-
+    private final PointService pointService;
     /**
      * TODO - 특정 유저의 포인트를 조회하는 기능을 작성해주세요.
      */
@@ -19,7 +22,10 @@ public class PointController {
     public UserPoint point(
             @PathVariable long id
     ) {
-        return new UserPoint(0, 0, 0);
+        UserPoint userPoint = pointService.getUserPoint(id);
+        log.debug("조회 결과: {}", userPoint);
+
+        return userPoint;
     }
 
     /**
@@ -29,7 +35,9 @@ public class PointController {
     public List<PointHistory> history(
             @PathVariable long id
     ) {
-        return List.of();
+        List<PointHistory> pointHistories = pointService.getUserPointHistories(id);
+        log.debug("조회 결과: {}", pointHistories);
+        return pointHistories;
     }
 
     /**
@@ -40,7 +48,9 @@ public class PointController {
             @PathVariable long id,
             @RequestBody long amount
     ) {
-        return new UserPoint(0, 0, 0);
+        UserPoint userPoint = pointService.chargePoint(id, amount);
+        log.debug("충전 결과: {}", userPoint);
+        return userPoint;
     }
 
     /**
@@ -51,6 +61,8 @@ public class PointController {
             @PathVariable long id,
             @RequestBody long amount
     ) {
-        return new UserPoint(0, 0, 0);
+        UserPoint userPoint = pointService.usePoint(id, amount);
+        log.debug("사용 결과: {}", userPoint);
+        return userPoint;
     }
 }
