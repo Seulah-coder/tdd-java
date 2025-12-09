761fef033235cd5e5c4c5546c7c9bbc3bbd4a875
Seulah-coder <seulah.developer@gmail.com>
2025-12-09 15:56:14 +0900

    포인트 충전 테스트 코드 작성

---
+++ src/test/java/io/code/tdd/point/PointServiceTest.java
@@ -0,0 +1,48 @@
+package io.code.tdd.point;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import io.code.tdd.database.UserPointTable;
+import io.code.tdd.database.PointHistoryTable;
+
+@ExtendWith(MockitoExtension.class)
+class PointServiceTest {
+
+    @Mock
+    private UserPointTable userPointTable;
+
+    @Mock
+    private PointHistoryTable pointHistoryTable;
+
+    @InjectMocks
+    private PointService pointService;
+
+    @Test
+    void addPointTest(){
+
+    //given
+    long userId = 1L;
+    long amount = 100L;
+
+    UserPoint before = new UserPoint(userId, 500L, System.currentTimeMillis());
+    UserPoint after = new UserPoint(userId, 600L, System.currentTimeMillis());
+
+    when(userPointTable.selectById(userId)).thenReturn(before);
+    when(userPointTable.insertOrUpdate(eq(userId), eq(600L))).thenReturn(after);
+
+    //when
+    UserPoint result = pointService.chargePoint(userId, amount);
+
+    //then
+    assertEquals(600L, result.point());
+    }
+
+    
+}
